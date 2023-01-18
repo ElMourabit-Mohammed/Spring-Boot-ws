017c1514e26987c3c551cff0e21823ba1c96aa37
ElMourabit-Mohammed <elmourabitmohammed00@gmail.com>
2023-01-18 13:30:34 +0100

    first commit

---
+++ src/main/java/com/myapp/ws/ws_app/WsAppApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.ws.ws_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myapp/ws/ws_app/WsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.ws.ws_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
