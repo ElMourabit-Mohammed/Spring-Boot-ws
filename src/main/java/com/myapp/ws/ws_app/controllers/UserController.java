dbf06bfa3b286996baefce844e5f3e13e2c91fa6
ElMourabit-Mohammed <elmourabitmohammed00@gmail.com>
2023-01-25 18:16:20 +0100

    Validation des données

--- src/main/java/com/myapp/ws/ws_app/controllers/UserController.java
+++ src/main/java/com/myapp/ws/ws_app/controllers/UserController.java
@@ -11,8 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
@@ -53,7 +55,7 @@ public class UserController {
 
     @PostMapping(consumes={MediaType.APPLICATION_XML_VALUE , MediaType.APPLICATION_JSON_VALUE},
                  produces={MediaType.APPLICATION_XML_VALUE , MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<UserResponse> addUser(@RequestBody UserRequest userRequest) throws Exception{
+    public ResponseEntity<UserResponse> addUser(@RequestBody @Valid UserRequest userRequest) throws Exception{
         if(userRequest.getFirstName().isEmpty()) throw new UserException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
 
         UserDto userDto = new UserDto();
--- src/main/java/com/myapp/ws/ws_app/requests/UserRequest.java
+++ src/main/java/com/myapp/ws/ws_app/requests/UserRequest.java
@@ -1,11 +1,27 @@
 package com.myapp.ws.ws_app.requests;
 
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class UserRequest {
 
+    @NotNull(message="Ce champ ne doit etre null !")
+    @Size(min=3, message="Ce champ doit avoir au moins 3 Caracteres !")
     private String firstName;
+    @NotNull(message="Ce champ ne doit etre null !")
+    @Size(min=3, message="Ce champ doit avoir au moins 3 Caracteres !")
     private String lastName;
+    @NotNull(message="Ce champ ne doit etre null !")
+    @Email(message="ce champ doit respecter le format email !")
     private String email;
+    @NotNull(message="Ce champ ne doit etre null !")
+    @Size(min=8, message="mot de passe doit avoir au moins 8 caracteres !")
+    @Size(max=12, message="mot de passe doit avoir au max 12 caracteres !")
+    @Pattern(regexp="(?=^.{8,}$)((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$",
+            message="ce mot de passe doit avoir des lettres en Maj et Minsc et numero")
     private String password;
 
     public String getFirstName() {
